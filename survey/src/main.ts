c9209a34c6b0443a7c72cdcaf783872045122f10
Kai Reeh <kaisreeh@gmail.com>
2022-09-30 00:09:14 +0200

    use PORT specified by env variables in backend

--- survey/src/main.ts
+++ survey/src/main.ts
@@ -2,9 +2,11 @@ import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
+const PORT = process.env.PORT || 8080
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     app.useGlobalPipes(new ValidationPipe({ transform: true, forbidNonWhitelisted: true }))
-    await app.listen(3001)
+    await app.listen(PORT)
 }
 bootstrap()
