aafb86f1df2eab81300b4164a0ca7bd82feeab4a
Kai Reeh <kaisreeh@gmail.com>
2022-10-03 23:31:10 +0200

    add simple known security to backend
    - added helmet to add security headers

--- survey/src/main.ts
+++ survey/src/main.ts
@@ -1,12 +1,15 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import helmet from 'helmet'
 
 const PORT = process.env.PORT || 8080
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     app.useGlobalPipes(new ValidationPipe({ transform: true, forbidNonWhitelisted: true }))
+    app.use(helmet())
     await app.listen(PORT)
+
 }
 bootstrap()
